92abe6b5133e7573c9bd63f1af5d41d41ccafb66
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-11 14:24:54 +0100

    exo2: compare images and send abs path if image is unique

--- exo2/main.go
+++ exo2/main.go
@@ -5,17 +5,42 @@ import (
 	"io/ioutil"
 	"log"
 	"crypto/sha256"
+	"bytes"
+	"path/filepath"
 )
 
-func main() {
-	// I'm reading my 'image_1.jpg' file
-	content, err := ioutil.ReadFile("image_1.jpg")
-	// If a have some issues
+func hashImg(path string) []byte{
+	content, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// else: I convert my byte in SHA256
 	h := sha256.New()
 	h.Write(content)
-	fmt.Printf("File contents: %x", h.Sum(nil))
+	return h.Sum(nil)
+}
+func main() {
+
+	file1_hash := hashImg("image_1.jpg")
+	file2_hash := hashImg("image_2.jpg")
+	file3_hash := hashImg("image_3.jpg")
+
+	res1 := bytes.Compare(file1_hash,file2_hash)
+	res2 := bytes.Compare(file1_hash,file3_hash)
+	res3 := bytes.Compare(file2_hash,file3_hash)
+
+	// if image 2 is unique
+	if  res1 != 0 && res3 != 0 {
+		fmt.Println("image_2 is unique")
+		fmt.Println(filepath.Abs("image_2.jpg"))
+	}
+	// if image 3 is unique
+	if res2 != 0 && res3 != 0{
+		fmt.Println("image_3 is unique") 
+		fmt.Println(filepath.Abs("image_3.jpg"))
+	}
+	// if image 1 is unique
+	if res1 != 0 && res2 != 0 {
+		fmt.Println("image_1 is unique") 
+		fmt.Println(filepath.Abs("image_1.jpg"))
+	}
 }
