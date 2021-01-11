63e4b7a54aeb8b09a96894b2e30497dcf250ee07
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-11 12:25:20 +0100

    add comment

--- exo2/main.go
+++ exo2/main.go
@@ -8,10 +8,13 @@ import (
 )
 
 func main() {
+	// I'm reading my 'image_1.jpg' file
 	content, err := ioutil.ReadFile("image_1.jpg")
+	// If a have some issues
 	if err != nil {
 		log.Fatal(err)
 	}
+	// else: I convert my byte in SHA256
 	h := sha256.New()
 	h.Write(content)
 	fmt.Printf("File contents: %x", h.Sum(nil))
