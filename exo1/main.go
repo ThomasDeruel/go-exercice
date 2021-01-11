d9604b11715ce449cb943d628a7708fb10773199
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-11 11:44:25 +0100

    add flag

--- exo1/main.go
+++ exo1/main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	const VERSION = "1.0"
-	myversion := flag.Bool("version",true, "show the program version")
+	myversion := flag.Bool("version",false, "show the program version")
 	flag.Parse()
 	if *myversion {
-		fmt.Println(myversion)
+		fmt.Println(VERSION)
 		return
 	}
 	fmt.Println("Hello World")
