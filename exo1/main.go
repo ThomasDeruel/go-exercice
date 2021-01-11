8b6f960c5ebca8c2c5f0fc8d92f1574c102185a1
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-11 11:42:54 +0100

    add flag (with issues)

--- exo1/main.go
+++ exo1/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"flag"
 )
 
 func main() {
-	fmt.Println("Bad")
-	fmt.Println("formatin")
+	const VERSION = "1.0"
+	myversion := flag.Bool("version",true, "show the program version")
+	flag.Parse()
+	if *myversion {
+		fmt.Println(myversion)
+		return
+	}
+	fmt.Println("Hello World")
+	
 }
