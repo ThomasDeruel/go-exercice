7921737ca1f47c483abf7d6bb37de65c64844ab6
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-11 16:46:47 +0100

    exo4: get images and upload in jpeg

---
+++ exo4/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"strings"
+	"net/http"
+	"log"
+	"strconv"
+	"io/ioutil"
+	//"os"
+	// "image/jpeg"
+	//"image/png"
+)
+
+func main(){
+	//const default_url = "http:url1.tld,http://url2.tld"
+	urls := flag.String("urls", "", "list of urls")
+	flag.Parse()
+	if *urls != "" {
+		// fmt.Println(*urls)
+		list_url := strings.Split(*urls,",")
+		for index, element := range list_url {
+			fmt.Println(element)
+			resp, err := http.Get(element)
+			if err != nil {
+				log.Fatal(err)
+				continue
+			}
+			data, err := ioutil.ReadAll(resp.Body)
+			if err != nil{
+				log.Fatal(err)
+			}
+			defer resp.Body.Close()
+			filename := "image-"+strconv.Itoa(index)+".jpeg"
+			ioutil.WriteFile(filename, data, 0644)
+		}
+		return
+	}
+	fmt.Print("urls is empty")
+}
