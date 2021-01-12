b38143106df576c81dd10cccba660acc4c344622
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-12 15:25:02 +0100

    exo6: partie 1 - with comment

--- exo6/main.go
+++ exo6/main.go
@@ -9,9 +9,12 @@ type MyError struct{
 	When time.Time
 	What string
 }
+// implementation, but ....
+// Wtf is " * "
 func (err *MyError) Error() string {
 	return fmt.Sprintf("When %s: \nWhat: %s", err.When, err.What)
 }
+// Wtf is " & "
 func run() error {
 	return &MyError{
 		When: time.Now(),
