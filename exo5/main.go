d2acd5f3282590d3bab68a9735ea086ba191a593
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-12 14:25:55 +0100

    exo5: done

--- exo5/main.go
+++ exo5/main.go
@@ -9,26 +9,20 @@ import (
 type IPAddr [4]byte
 
 
-// func(ip IPAddr) String() string {
-// 	res := []string{}
-// 	for _,val := range ip {
-// 		res = append(res,strconv.Itoa(int(val)))
-// 	}
-// 	return fmt.Printf("print: %v", strings.Join(res,"."))
-// }
+func(ip IPAddr)String() string {
+	res := []string{}
+	for _,val := range ip {
+		res = append(res,strconv.Itoa(int(val)))
+	}
+	stringval := strings.Join(res,".")
+	return fmt.Sprintf("print: %v", stringval)
+}
 func main() {
 	hosts := map[string]IPAddr{
 		"loopback":  {127, 0, 0, 1},
 		"googleDNS": {8, 8, 8, 8},
 	}
-	// for _,val := range hosts {
-	// 	fmt.Println(val)
-	// }
-	for _,ip := range hosts {
-		res := []string{}
-		for _,val := range ip {
-			res = append(res,strconv.Itoa(int(val)))
-		}
-		fmt.Printf("print: %v\n", strings.Join(res,"."))
+	for _,val := range hosts {
+		fmt.Println(val)
 	}
 }
