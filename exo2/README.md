bcaf22b0a8bb5b122252430124099339930df273
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-12 11:06:07 +0100

    Refactoring exercice 2

--- exo2/README.md
+++ exo2/README.md
@@ -3,3 +3,6 @@
 - Lire un fichier: avec la méthode `ReadFile` contenu dans `io/ioutil/`
 - Comparer 2 bytes: avec la méthode `Compare`contenu dans `bytes`
 - afficher le chemin abs d'un fichier: `Abs` dans `path/filepath`
+
+- version `2.0` : eaysy way
+- version `4.x.x`: hard way
--- exo2/main.go
+++ exo2/main.go
@@ -18,29 +18,50 @@ func hashImg(path string) []byte{
 	h.Write(content)
 	return h.Sum(nil)
 }
+
 func main() {
+	// we can defined var like this too
+	// var myVar mytype = someValue_withtype
+	
+	all_info := make(map[string][]byte)
 
-	file1_hash := hashImg("image_1.jpg")
-	file2_hash := hashImg("image_2.jpg")
-	file3_hash := hashImg("image_3.jpg")
-
-	res1 := bytes.Compare(file1_hash,file2_hash)
-	res2 := bytes.Compare(file1_hash,file3_hash)
-	res3 := bytes.Compare(file2_hash,file3_hash)
-
-	// if image 2 is unique
-	if  res1 != 0 && res3 != 0 {
-		fmt.Println("image_2 is unique")
-		fmt.Println(filepath.Abs("image_2.jpg"))
+	var (
+		files []string= []string{"image_1.jpg","image_2.jpg","image_3.jpg"}
+		files_hash [][]byte = [][]byte{}
+	)
+	for _,file := range files {
+		f_hash := hashImg(file)
+		files_hash = append(files_hash,f_hash)
+		all_info[file] = f_hash
 	}
-	// if image 3 is unique
-	if res2 != 0 && res3 != 0{
-		fmt.Println("image_3 is unique") 
-		fmt.Println(filepath.Abs("image_3.jpg"))
+
+	// find indes (duplicates byte)
+	indices := []int{}
+	for index,file_hash := range files_hash {
+		others_files_hash := files_hash[index+1:]
+		if len(others_files_hash) < 0 {
+			continue
+		}
+		for index2,file_hash_two := range others_files_hash{
+			res := bytes.Compare(file_hash,file_hash_two)
+			if res == 0 {
+				indices = append(indices,index,index2)
+			}	
+		}
 	}
-	// if image 1 is unique
-	if res1 != 0 && res2 != 0 {
-		fmt.Println("image_1 is unique") 
-		fmt.Println(filepath.Abs("image_1.jpg"))
+	// remove duplicate values
+	for i := range indices {
+		copy(files_hash[i:], files_hash[i+1:])
+		files_hash[len(files_hash)-1] = []byte{}  
+		files_hash = files_hash[:len(files_hash)-1] 
+	}
+	
+	for text,info := range all_info {
+		for _,file := range files_hash {
+			if bytes.Compare(file,info) == 0 {
+				fmt.Printf("Image unique: %s\n", text)
+				fmt.Println(filepath.Abs(text))
+			}
+		}
 	}
 }
