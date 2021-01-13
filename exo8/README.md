d1b1fc1b16963fe122ecb6e661647d22c13df1d5
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-13 10:20:36 +0100

    exo8: party one done

---
+++ exo8/README.md
@@ -0,0 +1,4 @@
+# Go exercice 8
+
+- `json.Marshal`: Retourne un JSON encodé (en byte)
+- on utilise `Stdout` (communication de sortie) pour afficher notre donnée en byte pour avoir le rendu en json
---
+++ exo8/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	//"fmt"
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type User struct {
+	Login    string
+	Password string
+}
+
+func main() {
+	var Paul = User{"Paul", "pass123"}
+	paul_injson, err := json.Marshal(Paul)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// stdout: sortie standard ->
+	os.Stdout.Write(paul_injson)
+}
