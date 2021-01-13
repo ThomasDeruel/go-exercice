44792f833216d396904c8410b421bf4ab98f9e62
thomas deruel <thomas.deruel@supdevinci-edu.fr>
2021-01-13 11:37:05 +0100

    exo8: done (with bonus)

--- exo8/main.go
+++ exo8/main.go
@@ -8,21 +8,30 @@ import (
 )
 
 type User struct {
-	userName string
-	Password string
+	UserName string `json:"username"`
+	Password string `json:"password"`
 }
 
 func main() {
+	var (
+		users []User
+		outputUsers =  make(map[string]User)
+	)
 	data, err := ioutil.ReadFile("./users.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	var users []User
 	err = json.Unmarshal(data, &users)
+	for index,user := range users {
+		i := fmt.Sprintf("%v",index)
+
+		outputUsers["user-"+i] = user
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%+v", users)
+	fmt.Println(outputUsers)
+
 	/*
 		output:
 
