29ddedf9a3260b0225a5e1605fd4ae0b3e0aedda
thomas deruel <thomas.deruel@hetic.net>
2021-01-14 16:39:42 +0100

    exo12: issue

---
+++ exo12/client/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	// "io/ioutil"
+	"bytes"
+	"log"
+	"flag"
+	"strings"
+	//"os"
+	"encoding/json"
+	// "crypto/sha256"
+)
+type typeURLs struct {
+	Urls []string `json:"urls,omitempty"`
+}
+func main(){
+	var list_url typeURLs
+	urls := flag.String("urls", "", "send list urls"); 
+	flag.Parse()
+	if *urls != "" {
+		list_url = typeURLs{strings.Split(*urls,",")}
+		urls_byte, err := json.Marshal(list_url)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		reader := bytes.NewReader(urls_byte)
+		// response, er => get response
+		_, er := http.Post(
+			"http://localhost:80",
+			"appplication/json",
+			reader,
+		)
+		if er != nil {
+			log.Fatal(er)
+			return
+		}
+		fmt.Println("ok")
+		return
+	}
+	fmt.Println("-urls is empty")
+}
